78de942fa4f469004cd02e97144a81dfbb870312
ilyashn2001-code <ilyas.hn.2001@gmail.com>
2025-09-24 17:15:13 +0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('themeToggle');
-  toggle.addEventListener('click', () => {
-    const root = document.documentElement;
-    const current = root.getAttribute('data-theme');
+document.addEventListener('DOMContentLoaded', function () {
+  // Переключение темы
+  const themeToggle = document.getElementById('themeToggle');
+
+  themeToggle.addEventListener('click', () => {
+    const current = document.documentElement.getAttribute('data-theme');
     const next = current === 'dark' ? 'light' : 'dark';
-    root.setAttribute('data-theme', next);
+    document.documentElement.setAttribute('data-theme', next);
     localStorage.setItem('theme', next);
   });
 
